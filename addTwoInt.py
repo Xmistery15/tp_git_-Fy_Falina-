6bf2f75bf272ee28feab5fdec50c3e55a6b8a997
Xmistery15 <fyfalinarandrianarijaona@gmail.com>
2022-07-18 11:07:35 +0400

    [0.1add]addTwoInt.py modifié pour afficher un message d'erreur

--- addTwoInt.py
+++ addTwoInt.py
@@ -7,6 +7,12 @@ print(sys.argv)
 def add(a,b):
 	return a+b
 
+#tester si le nombre d'argument est correct
+#et afficher un message d'erreur sinon
+nbr_arg=len(sys.argv)-1
+if (nbr_arg != 2):
+	print("Assurez-vous de mettre en argument les 2  chiffres à ajouter")
+
 #main qui va tester la fonction
 
 x=int(sys.argv[1])
